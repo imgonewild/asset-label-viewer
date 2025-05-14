47e6919de3b8ce72ad884111d7aa91602f0127df
unknown <kaisite2004@gmail.com>
2025-05-14 16:28:31 +0800

    add ssl & upload example csv

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,30 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import fs from "fs";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode, command }) => {
+  const isSSL = mode === "ssl";
+
+  return {
+    server: {
+      host: "::",
+      port: 8080,
+      ...(isSSL && {
+        https: {
+          key: fs.readFileSync(path.resolve(__dirname, "cert/key.pem")),
+          cert: fs.readFileSync(path.resolve(__dirname, "cert/cert.pem")),
+        },
+      }),
     },
-  },
-}));
+    plugins: [react(), mode === "development" && componentTagger()].filter(
+      Boolean
+    ),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+});
